5e5d1909759a2a2d67a43032b95d17143d120f25
Atomis14 <michelsabbatini@outlook.com>
2024-07-10 19:16:37 +0200

    compile with signle command

--- compile.py
+++ compile.py
@@ -16,19 +16,21 @@ def run_and_print(command):
 def compile_and_flash():
   """Compile and flash the project raw without secure boot or flash encryption."""
   commands = (
-    # set the target ESP architecture
-    ["idf.py", "--project-dir", project_path, "set-target", target],
+    # initialize the esp-idf environment
+    "./run_with_env.sh", esp_idf_export_script_path,
+    # call esp idf
+    "idf.py",
+    # define directory of the project and port for flashing
+    "--project-dir", project_path, "--port", port,
+    # set the target architecture (e.g. esp32 or esp32s3)
+    "set-target", target,
     # build the binary
-    ["idf.py", "--project-dir", project_path, "build"],
-    # flash the binary to the ESP
-    ["idf.py", "--project-dir", project_path, "--port", port, "flash"]
+    "build",
+    # flash the binary to the specified port
+    "flash"
   )
-
   start = time.time()
-  for command in commands:
-    idf_init_command = ["./run_with_env.sh", esp_idf_export_script_path]  # initialize the esp-idf environment
-    command = idf_init_command + command
-    run_and_print(command)
+  run_and_print(commands)
   print(time.time()-start)
 
 compile_and_flash()
