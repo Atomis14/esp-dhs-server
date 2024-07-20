bcc18d7e4a143cbba4b27610920b0064418d5a0d
Atomis14 <michelsabbatini@outlook.com>
2024-07-20 16:08:33 +0200

    move models to own file

--- database.py
+++ database.py
@@ -1,29 +1,8 @@
-from sqlalchemy import ForeignKey, create_engine, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, relationship
-from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import Base, Message, Configuration
 
 def _setup_database():
-  class Base(DeclarativeBase):
-    pass
-
-  class Message(Base):
-    __tablename__ = 'messages'
-    message_id:     Mapped[int]             = mapped_column(primary_key=True)
-    created_at:     Mapped[datetime]        = mapped_column(server_default=func.now())
-    type:           Mapped[str]
-    configuration:  Mapped["Configuration"] = relationship(back_populates="message")
-
-    def __repr__(self) -> str:
-      return f"• Message(id: {self.message_id}, created_at: {self.created_at}, type: {self.type})"
-
-  class Configuration(Base):
-    __tablename__ = 'configurations'
-    configuration_id: Mapped[int]       = mapped_column(primary_key=True)
-    message_id:       Mapped[int]       = mapped_column(ForeignKey("messages.message_id"), unique=True)
-    message:          Mapped["Message"] = relationship(back_populates="configuration", single_parent=True)
-
-    def __repr__(self) -> str:
-      return f"• Configuration(id: {self.configuration_id}, message: {self.message})"
 
   engine = create_engine('sqlite:///database.sqlite3', echo=True)
 
---
+++ models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from datetime import datetime
+
+
+class Base(DeclarativeBase):
+  pass
+
+
+class Configuration(Base):
+  __tablename__ = 'configurations'
+  configuration_id: Mapped[int]       = mapped_column(primary_key=True)
+  message_id:       Mapped[int]       = mapped_column(ForeignKey("messages.message_id"), unique=True)
+  message:          Mapped["Message"] = relationship(back_populates="configuration", single_parent=True)
+
+  def __repr__(self) -> str:
+    return f"• Configuration(id: {self.configuration_id}, message: {self.message})"
+
+
+class Message(Base):
+  __tablename__ = 'messages'
+  message_id:     Mapped[int]             = mapped_column(primary_key=True)
+  created_at:     Mapped[datetime]        = mapped_column(server_default=func.now())
+  type:           Mapped[str]
+  configuration:  Mapped["Configuration"] = relationship(back_populates="message")
+
+  def __repr__(self) -> str:
+    return f"• Message(id: {self.message_id}, created_at: {self.created_at}, type: {self.type})"
